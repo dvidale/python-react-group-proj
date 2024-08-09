c98126c1a15bb9b9660a11736127a83d32ab4190
Hayden <haydengalyeanbiz@gmail.com>
2024-08-09 19:40:04 -0400

    built Restaurant seeder file and added funcs to seeder __init__.py

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .restaurant import seed_rest, undo_rest
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,7 +18,9 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_rest()
     seed_users()
+    seed_rest()
     # Add other seed functions here
 
 
@@ -25,4 +28,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_rest()
     # Add other undo functions here
--- migrations/versions/7a80fac2f31a_init_migration.py
+++ migrations/versions/7a80fac2f31a_init_migration.py
@@ -1,8 +1,8 @@
 """init migration
 
-Revision ID: 59c6f8743170
+Revision ID: 7a80fac2f31a
 Revises: 
-Create Date: 2024-08-09 19:22:39.398422
+Create Date: 2024-08-09 19:37:26.818111
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '59c6f8743170'
+revision = '7a80fac2f31a'
 down_revision = None
 branch_labels = None
 depends_on = None
