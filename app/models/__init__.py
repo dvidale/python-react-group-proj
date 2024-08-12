31a93fbce9570af9e4882f7d94a48b7976db42f1
DeAndre <deandre.vidale@gmail.com>
2024-08-11 22:14:17 -0700

    put restaurant category before category and restaurant in model init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,11 @@
 from .db import db, environment, SCHEMA
-from .category import Category
 from .user import User
 from .shopping_cart import ShoppingCart
 from .cart_item import CartItem
-from .restaurant import Restaurant
-from .reviews import Review
 from .restaurant_category import RestaurantCategory
+from .restaurant import Restaurant
+from .category import Category
+from .reviews import Review
 from .menu_item import MenuItem
 from .menu_item_rating import MenuItemRating
 
