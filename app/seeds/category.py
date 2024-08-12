8ce5c2f5087de22ab164a13a396a5d4e2cebe8ce
DeAndre <deandre.vidale@gmail.com>
2024-08-11 22:30:36 -0700

    moved restaurant categories above restaurants in model migration

--- app/seeds/category.py
+++ app/seeds/category.py
@@ -21,5 +21,5 @@ def seed_categories():
     db.session.commit()
 
 def undo_categories():
-    db.session.execute('TRUNCATE restaurant_categories, categories RESTART IDENTITY CASCADE;')
+    db.session.execute('TRUNCATE categories RESTART IDENTITY CASCADE;')
     db.session.commit()
