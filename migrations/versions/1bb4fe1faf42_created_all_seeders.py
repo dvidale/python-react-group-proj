3e1c1eb9d83c7eaaab8fb052c29208624024fa34
DeAndre <deandre.vidale@gmail.com>
2024-08-11 17:08:09 -0700

    renamed new migration to match missing file name

--- migrations/versions/1bb4fe1faf42_created_all_seeders.py
+++ migrations/versions/1bb4fe1faf42_created_all_seeders.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 7e9485248027
+Revision ID: 1bb4fe1faf42
 Revises: 
-Create Date: 2024-08-11 16:12:46.340338
+Create Date: 2024-08-11 17:01:53.601436
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7e9485248027'
+revision = '1bb4fe1faf42'
 down_revision = None
 branch_labels = None
 depends_on = None
