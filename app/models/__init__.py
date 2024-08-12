8032a430c7bef68105280acf6685311d5687be22
DeAndre <deandre.vidale@gmail.com>
2024-08-11 22:34:59 -0700

    moved restaurant import below category and restaurant category in model init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,8 @@ from .user import User
 from .shopping_cart import ShoppingCart
 from .cart_item import CartItem
 from .restaurant_category import RestaurantCategory
-from .restaurant import Restaurant
 from .category import Category
+from .restaurant import Restaurant
 from .reviews import Review
 from .menu_item import MenuItem
 from .menu_item_rating import MenuItemRating
