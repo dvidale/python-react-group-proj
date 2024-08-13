3cd963b389b87b664d16f6983c5a22af905bad8c
Neil <neil.kangaroo@gmail.com>
2024-08-12 20:16:21 -0500

    Pushing to fix Neil's merges to dev - Review Routes not showing up

--- app/api/menu_items.py
+++ app/api/menu_items.py
@@ -7,5 +7,6 @@ menuitem_routes = Blueprint('menu-items', __name__)
 @menuitem_routes.route('/<int:menuitem_id>/ratings')
 def get_menu_item_ratings(menuitem_id):
     menu_item = MenuItem.query.get(menuitem_id)
+    ratings = menu_item.reviews
 
-    return menu_item.to_dict_ratings()
+    return ratings.to_dict()
