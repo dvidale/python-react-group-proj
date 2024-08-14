f6ef3f0cf9d86d1b972edb0c673e9fd7f17cfdbc
Hayden <haydengalyeanbiz@gmail.com>
2024-08-14 19:07:29 -0400

    nightly commit

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -21,7 +21,13 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), username_exists])
+    email = StringField('Email', validators=[DataRequired(), Email(), user_exists])
+    address = StringField('Address', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    state = StringField('State', validators=[DataRequired(), Length(min=2, max=2)])
+    zip = StringField('ZIP', validators=[DataRequired()])
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired(), Length(min=6)])
--- react-vite/src/components/SignupFormModal/SignupFormModal.jsx
+++ react-vite/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,92 +1,186 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import { thunkSignup } from "../../redux/session";
-import "./SignupForm.css";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import { thunkSignup } from '../../redux/session';
+import './SignupForm.css';
 
 function SignupFormModal() {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState({});
-  const { closeModal } = useModal();
+	const dispatch = useDispatch();
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [address, setAddress] = useState('');
+	const [city, setCity] = useState('');
+	const [state, setState] = useState('');
+	const [zip, setZip] = useState('');
+	const [phoneNumber, setPhoneNumber] = useState('');
+	const [errors, setErrors] = useState({});
+	const { closeModal } = useModal();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    if (password !== confirmPassword) {
-      return setErrors({
-        confirmPassword:
-          "Confirm Password field must be the same as the Password field",
-      });
-    }
+		if (password !== confirmPassword) {
+			return setErrors({
+				confirmPassword:
+					'Confirm Password field must be the same as the Password field',
+			});
+		}
 
-    const serverResponse = await dispatch(
-      thunkSignup({
-        email,
-        username,
-        password,
-      })
-    );
+		const csrfToken = document.querySelector('input[name="csrf_token"]').value;
 
-    if (serverResponse) {
-      setErrors(serverResponse);
-    } else {
-      closeModal();
-    }
-  };
+		const serverResponse = await dispatch(
+			thunkSignup({
+				firstName,
+				lastName,
+				email,
+				username,
+				password,
+				address,
+				city,
+				state,
+				zip,
+				phoneNumber,
+				csrf_token: csrfToken,
+			})
+		);
 
-  return (
-    <>
-      <h1>Sign Up</h1>
-      {errors.server && <p>{errors.server}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
-      </form>
-    </>
-  );
+		if (serverResponse) {
+			setErrors(serverResponse);
+		} else {
+			closeModal();
+		}
+	};
+
+	return (
+		<>
+			<h1>Sign Up</h1>
+			{errors.server && <p>{errors.server}</p>}
+			<form onSubmit={handleSubmit}>
+				<input
+					type='hidden'
+					name='csrf_token'
+					value={document
+						.querySelector('meta[name="csrf-token"]')
+						.getAttribute('content')}
+				/>
+				<label>
+					First Name
+					<input
+						type='text'
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.firstName && <p>{errors.firstName}</p>}
+				<label>
+					Last Name
+					<input
+						type='text'
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.lastName && <p>{errors.lastName}</p>}
+				<label>
+					Email
+					<input
+						type='text'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.email && <p>{errors.email}</p>}
+				<label>
+					Username
+					<input
+						type='text'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.username && <p>{errors.username}</p>}
+				<label>
+					Password
+					<input
+						type='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.password && <p>{errors.password}</p>}
+				<label>
+					Confirm Password
+					<input
+						type='password'
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+				<label>
+					Address
+					<input
+						type='text'
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.address && <p>{errors.address}</p>}
+				<label>
+					City
+					<input
+						type='text'
+						value={city}
+						onChange={(e) => setCity(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.city && <p>{errors.city}</p>}
+				<label>
+					State
+					<input
+						type='text'
+						value={state}
+						onChange={(e) => setState(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.state && <p>{errors.state}</p>}
+				<label>
+					ZIP
+					<input
+						type='text'
+						value={zip}
+						onChange={(e) => setZip(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.zip && <p>{errors.zip}</p>}
+				<label>
+					Phone Number
+					<input
+						type='text'
+						value={phoneNumber}
+						onChange={(e) => setPhoneNumber(e.target.value)}
+						required
+					/>
+				</label>
+				{errors.phoneNumber && <p>{errors.phoneNumber}</p>}
+				<button type='submit'>Sign Up</button>
+			</form>
+		</>
+	);
 }
 
 export default SignupFormModal;
