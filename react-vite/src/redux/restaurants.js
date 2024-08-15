390a1f73c1d6ff48bdacffe9435942f3cf681ac7
DeAndre <deandre.vidale@gmail.com>
2024-08-15 14:12:52 -0700

    consolidated new/update restaurant action creator and reducers into one

--- react-vite/src/redux/restaurants.js
+++ react-vite/src/redux/restaurants.js
@@ -6,7 +6,8 @@ const GET_RESTAURANTS = '/restaurants/GET_RESTAURANTS';
 
 const GET_A_RESTAURANT = 'restaurants/GET_A_RESTAURANT';
 
-const ADD_NEW_RESTAURANT = '/restaurants/ADD_NEW_RESTAURANT';
+const ADD_UPDATE_RESTAURANT = '/restaurants/ADD_OR_UPDATE_RESTAURANT';
+
 
 //*-----------------------------------ACTION CREATORS
 
@@ -31,9 +32,9 @@ export function getARestaurant(data) {
 	};
 }
 
-export function addNewRestaurant(data){
+export function add_Or_Update_Restaurant(data){
 	return {
-		type: ADD_NEW_RESTAURANT,
+		type: ADD_UPDATE_RESTAURANT,
 		payload: data
 	}
 }
@@ -95,12 +96,12 @@ export const newRestaurant = (method, formData) => async (dispatch) =>{
 
 	const data = await response.json()
 	
-	dispatch(addNewRestaurant(data))
+	dispatch(add_Or_Update_Restaurant(data))
 
 	return data
 }
 
-export const updateRestaurant = (id, method, formData) => async () => {
+export const updateRestaurant = (id, method, formData) => async (dispatch) => {
 
 	const url = `/api/restaurants/current/${id}`
 	const headers = {'Content-Type': 'application/json'}
@@ -111,8 +112,9 @@ export const updateRestaurant = (id, method, formData) => async () => {
 	
 	const data = await response.json()
 
+
 	console.log(">>> data from flask POST route:", data)
-	
+	dispatch(add_Or_Update_Restaurant(data))
 	return data
 
 
@@ -148,12 +150,12 @@ const restaurantsReducer = (state = initialState, action) => {
 			return { ...state, selectedRestaurant: action.payload };
 
 			
-		case ADD_NEW_RESTAURANT:{
+		case ADD_UPDATE_RESTAURANT:{
 			const newState = {...state}
 
-			const new_restaurant = action.payload;
+			const restaurant = action.payload;
 
-			newState.AllRestaurants[new_restaurant.id] = new_restaurant;
+			newState.AllRestaurants[restaurant.id] = restaurant;
 
 			return newState
 		}	
