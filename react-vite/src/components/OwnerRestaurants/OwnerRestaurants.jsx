a8b3e8b377f57bdff55c4af876af12db136d8988
Hayden <haydengalyeanbiz@gmail.com>
2024-08-16 11:28:00 -0400

    working restaurant portal filter

---
+++ react-vite/src/components/OwnerRestaurants/OwnerRestaurants.jsx
@@ -0,0 +1,51 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getRestaurants } from '../../redux/restaurants';
+
+function OwnerRestaurants({ city, state }) {
+	const dispatch = useDispatch();
+	const owner = useSelector((state) => state.session.user);
+	const all_restaurants = useSelector(
+		(state) => state.restaurants.AllRestaurants
+	);
+
+	useEffect(() => {
+		dispatch(getRestaurants());
+	}, [dispatch]);
+
+	// Filter restaurants by owner_id
+	const filteredRestaurants = Object.values(all_restaurants).filter(
+		(restaurant) => restaurant.owner_id === owner.id
+	);
+
+	return (
+		<div className='restaurant-list'>
+			{filteredRestaurants.map((restaurant) => (
+				<div
+					key={restaurant.id}
+					className='restaurant-tile'
+				>
+					<img
+						src={restaurant.banner_img}
+						alt={restaurant.name}
+						className='restaurant-image'
+					/>
+					<div className='restaurant-info'>
+						<h2>{restaurant.name}</h2>
+						<p>{restaurant.avg_rating}</p>
+						<p>{restaurant.categories.join(' • ')}</p>
+						<p>{restaurant.description}</p>
+						{(city && state) || (restaurant.city && restaurant.state) ? (
+							<div className='restaurant-address'>
+								{restaurant.address}, {city || restaurant.city},{' '}
+								{state || restaurant.state}
+							</div>
+						) : null}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default OwnerRestaurants;
