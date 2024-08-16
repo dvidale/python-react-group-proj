0eacc739d48a02c1aaea606b0e10f81ed7a75aca
DeAndre <deandre.vidale@gmail.com>
2024-08-15 20:39:26 -0700

    re-added original migration for alembic history

--- migrations/versions/0abc4a4cecf9_.py
+++ migrations/versions/0abc4a4cecf9_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '0abc4a4cecf9'
-down_revision = None
+down_revision = '1bb4fe1faf42'
 branch_labels = None
 depends_on = None
 
