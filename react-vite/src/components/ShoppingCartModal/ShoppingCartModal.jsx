ea5ff2209da59b6dd201dd299ee6524db00db8c8
Hayden <haydengalyeanbiz@gmail.com>
2024-08-19 00:14:35 -0400

    fixed shopping cart styling

--- react-vite/src/components/ShoppingCartModal/ShoppingCartModal.jsx
+++ react-vite/src/components/ShoppingCartModal/ShoppingCartModal.jsx
@@ -15,7 +15,7 @@ const ShoppingCartModal = () => {
 	const sessionUser = useSelector((state) => state.session.user);
 
 	const handleAddToCart = (menuItemId) => {
-		dispatch(addCartItem(menuItemId))
+		dispatch(addCartItem(menuItemId));
 	};
 
 	const handleRemoveItem = (itemId) => {
@@ -36,9 +36,14 @@ const ShoppingCartModal = () => {
 
 	return (
 		<div className='shopping-cart-modal'>
-			<div>
+			<div className='shop-header'>
 				<h2>Your Shopping Cart</h2>
-				<button onClick={closeModal}>Close</button>
+				<button
+					className='close-shop-btn'
+					onClick={closeModal}
+				>
+					x
+				</button>
 			</div>
 			{shoppingCart.length > 0 ? (
 				<>
@@ -55,15 +60,23 @@ const ShoppingCartModal = () => {
 									/>
 									<div>
 										<h3>{item.name}</h3>
-										<p>{item.description}</p>
-										<p>Price: ${item.price}</p>
+										<p className='shop-desc'>{item.description}</p>
+										<p className='shop-price'>Price: ${item.price}</p>
 									</div>
 								</div>
 								<div className='cart-quantity'>
-									<p className='quantity'>{item.item_quantity}</p>{' '}
+									<p className='quantity'>Qty: {item.item_quantity}</p>{' '}
 									<div className='quantity-btns'>
-										<button onClick={() => handleAddToCart(item)}>+</button>
-										<button onClick={() => handleRemoveItem(item.id)}>
+										<button
+											className='shop-add-btn'
+											onClick={() => handleAddToCart(item)}
+										>
+											+
+										</button>
+										<button
+											className='shop-delete-btn'
+											onClick={() => handleRemoveItem(item.id)}
+										>
 											Delete
 										</button>
 									</div>
@@ -75,7 +88,14 @@ const ShoppingCartModal = () => {
 			) : (
 				<p>Your cart is empty.</p>
 			)}
-			{sessionUser && <button onClick={handlePurchase}>Purchase</button>}
+			{sessionUser && (
+				<button
+					className='shop-purchase-btn'
+					onClick={handlePurchase}
+				>
+					Purchase
+				</button>
+			)}
 		</div>
 	);
 };
