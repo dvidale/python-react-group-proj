87558276f776916f18cc1019f84a846ea2287571
Hayden <haydengalyeanbiz@gmail.com>
2024-08-19 10:17:13 -0400

    added some styling to fix the bugs

--- react-vite/src/components/RestaurantInfoText/RestaurantInfoText.jsx
+++ react-vite/src/components/RestaurantInfoText/RestaurantInfoText.jsx
@@ -4,8 +4,10 @@ function RestaurantInfoText({ restaurant, city, state }) {
 			<h1 className='header-h1-title'>{restaurant.name}</h1>
 			<div>
 				<p>
-					{/* {restaurant.average_rating.toFixed(1) > 0.0 ? restaurant.average_rating.toFixed(1) : "New"} •  */}
-					{restaurant.categories.join(' • ')}
+					{restaurant.average_rating.toFixed(1) > 0.0
+						? restaurant.average_rating.toFixed(1)
+						: 'New'}{' '}
+					•{restaurant.categories.join(' • ')}
 					<br />
 					{restaurant.address}
 					<br />
